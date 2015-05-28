181a2829966ef9b9dbfb9af55d9c77a1bee6565f
Claudson Martins <claudson@outlook.com>
2015-05-28 16:23:16 -0300

    Update main script
    - Script to make changes on selected tabs

--- js/main.js
+++ js/main.js
@@ -5,6 +5,19 @@ jQuery(function($) {
 		$(this).css('margin-top',  ($('#main-slider').height() - $(this).height())/2);
 	});
 
+	// Launching the right tab section on click on index button
+	$('#main-slider button[data-toggle="modal"]').click(function(e) {
+		e.preventDefault();
+		var tab = ($(this).text() == 'CADASTRAR') ? "#register" : '#login';
+		$('#modal-tab a[href="'+ tab + '"]').click();
+	});
+
+	// Changing modal header according to tab section
+	$('#modal-tab a').click(function (e) {
+		var headerText = ($(this).attr('href') == '#register') ? 'Crie a sua conta' : 'Entre na sua conta';
+		$('#login-register .modal-header h2').text(headerText);
+	});
+
 	//contact form
 	var form = $('.subscribe-form');
 	form.submit(function () {
